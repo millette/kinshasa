ca4c873b0940b033b87f3c6d68395a6a142ba0a2
Robin Millette <robin@millette.info>
2018-07-11 04:51:10 -0400

    Pretty.

--- src/components/github-user.js
+++ src/components/github-user.js
@@ -1,11 +1,14 @@
 import React from 'react'
 
-const GithubUser = (props) => {
+const GithubUser = props => {
   const languages = {}
 
   props.repositoriesContributedTo.edges.forEach(({ node }) => {
-    const primaryLanguage = node && node.primaryLanguage && node.primaryLanguage.name
-    if (!primaryLanguage) { return }
+    const primaryLanguage =
+      node && node.primaryLanguage && node.primaryLanguage.name
+    if (!primaryLanguage) {
+      return
+    }
     if (!languages[primaryLanguage]) {
       languages[primaryLanguage] = 0
     }
@@ -14,41 +17,57 @@ const GithubUser = (props) => {
 
   const l2 = []
   let r
-  for (r in languages) { l2.push([r, languages[r]]) }
+  for (r in languages) {
+    l2.push([r, languages[r]])
+  }
 
   // sort by number of appearances
-  const l3 = l2.sort((a, b) => {
-    if (a[1] > b[1]) { return 1 }
-    if (a[1] < b[1]) { return -1 }
-    return 0
-  })
+  const l3 = l2
+    .sort((a, b) => {
+      if (a[1] > b[1]) {
+        return 1
+      }
+      if (a[1] < b[1]) {
+        return -1
+      }
+      return 0
+    })
     .reverse()
     // convert to a string
-    .map((x) => `${x[0]} (${x[1]})`)
+    .map(x => `${x[0]} (${x[1]})`)
     .join(', ')
 
-  return <div style={{ padding: '1em', margin: '1em', border: 'thin solid blue' }}>
-    <h2>{props.name || props.login}</h2>
-    {props.bio && <p>{props.bio}</p>}
-    <dl>
-      <dt>Location</dt>
-      <dd>{props.location}</dd>
-      <dt>Site web</dt>
-      <dd>{props.websiteUrl}</dd>
-      <dt>Créé</dt>
-      <dd>{props.createdAt}</dd>
+  return (
+    <div style={{ padding: '1em', margin: '1em', border: 'thin solid blue' }}>
+      <h2>{props.name || props.login}</h2>
+      {props.bio && <p>{props.bio}</p>}
+      <dl>
+        <dt>Location</dt>
+        <dd>{props.location}</dd>
+        <dt>Site web</dt>
+        <dd>{props.websiteUrl}</dd>
+        <dt>Créé</dt>
+        <dd>{props.createdAt}</dd>
 
-      <dt>Étoiles</dt>
-      <dd>{props.starredRepositories && props.starredRepositories.edges.length} dépots</dd>
+        <dt>Étoiles</dt>
+        <dd>
+          {props.starredRepositories && props.starredRepositories.edges.length}{' '}
+          dépots
+        </dd>
 
-      <dt>Contribué à</dt>
-      <dd>{props.repositoriesContributedTo && props.repositoriesContributedTo.edges.length} dépots</dd>
+        <dt>Contribué à</dt>
+        <dd>
+          {props.repositoriesContributedTo &&
+            props.repositoriesContributedTo.edges.length}{' '}
+          dépots
+        </dd>
 
-      <dt>Languages</dt>
-      <dd>{l3}</dd>
-    </dl>
-    {props.isHireable && <p>Disponible pour travailler</p>}
-  </div>
+        <dt>Languages</dt>
+        <dd>{l3}</dd>
+      </dl>
+      {props.isHireable && <p>Disponible pour travailler</p>}
+    </div>
+  )
 }
 
 export default GithubUser
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -3,59 +3,63 @@ import Link from 'gatsby-link'
 
 import GithubUser from '../components/github-user'
 
-const SecondPage = ({ data }) => <div>
-  <h1>Hi from the second page</h1>
-  <p>Welcome to page 2</p>
-  <Link to="/">Go back to the homepage</Link>
+const SecondPage = ({ data }) => (
   <div>
-    {data.allGithubSearch.edges.filter((x) => x.node && x.node.login).map((x) => <GithubUser key={x.node.id} { ...x.node } />)}
+    <h1>Hi from the second page</h1>
+    <p>Welcome to page 2</p>
+    <Link to="/">Go back to the homepage</Link>
+    <div>
+      {data.allGithubSearch.edges
+        .filter(x => x.node && x.node.login)
+        .map(x => <GithubUser key={x.node.id} {...x.node} />)}
+    </div>
   </div>
-</div>
+)
 
 export default SecondPage
 
 export const pageQuery = graphql`
   query MyQueryGitHub {
-  allGithubSearch {
-    edges {
-      node {
-        id
-        userCount
-        name
-        bio
-        login
-        databaseId
-        location
-        createdAt
-        isHireable
-        websiteUrl
-        starredRepositories {
-          edges {
-            starredAt
-            node {
-              nameWithOwner
-              primaryLanguage {
-                name
+    allGithubSearch {
+      edges {
+        node {
+          id
+          userCount
+          name
+          bio
+          login
+          databaseId
+          location
+          createdAt
+          isHireable
+          websiteUrl
+          starredRepositories {
+            edges {
+              starredAt
+              node {
+                nameWithOwner
+                primaryLanguage {
+                  name
+                }
               }
             }
           }
-        }
-        repositoriesContributedTo {
-          edges {
-            node {
-              forkCount
-              stargazers {
-                totalCount
+          repositoriesContributedTo {
+            edges {
+              node {
+                forkCount
+                stargazers {
+                  totalCount
+                }
+                primaryLanguage {
+                  name
+                }
+                nameWithOwner
               }
-              primaryLanguage {
-                name
-              }
-              nameWithOwner
             }
           }
         }
       }
     }
   }
-}
 `
